b9a02962c4fe42c009dc72914fb47a3af10fd436
kmagdi <katay.magdi@gmail.com>
2020-12-21 13:36:05 +0100

    noutfound.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import {Footer} from './components/Footer'
 import { FilterBar } from './components/FilterBar'
 import {BrowserRouter,Route,Switch} from "react-router-dom"
 import { Detail} from './components/Detail';
+import {NotFound} from './components/NotFound'
 //import "./App.css"
 
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/jobportal">
        <Navbar />
       
          <Switch>
@@ -17,6 +18,7 @@ export const App = () => {
           <Route path="/" exact component={ FilterBar}/>
           <Route path="/jobportal"  component={ FilterBar}/>
            <Route path="/detail/:id,:jobName" exact component={Detail}/>
+           <Route component={NotFound} />
         </Switch>
         <Footer/>
     </BrowserRouter>
---
+++ src/components/NotFound.js
@@ -0,0 +1,5 @@
+export const NotFound=()=>{
+    return(
+        <h1> ...ez az oldal nem található...</h1>
+    )
+}
