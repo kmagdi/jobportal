11ba7d46e3729252f4db21f6380d1e3660854854
kmagdi <katay.magdi@gmail.com>
2020-12-22 22:07:04 +0100

    form

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import {Footer} from './components/Footer'
 import { FilterBar } from './components/FilterBar'
 import {BrowserRouter,Route,Switch,Redirect} from "react-router-dom"
 import { Detail} from './components/Detail';
-import {NotFound} from './components/NotFound'
+
 //import "./App.css"
 
 
 export const App = () => {
   return (
-    <BrowserRouter basename="/jobportal">>
+    <BrowserRouter basename="/jobportal">
        <Navbar />
       
          <Switch>
-          
           <Route path="/" exact component={ FilterBar}/>
           <Route path="/jobportal"  component={ FilterBar}/>
            <Route path="/detail/:id,:jobName" exact component={Detail}/>
            <Route path="/jobportal/detail/:id,:jobName" exact component={Detail}/>
            <Redirect to="/" />
         </Switch>
+
         <Footer/>
     </BrowserRouter>
    )  
--- src/components/NotFound.js
+++
@@ -1,9 +0,0 @@
-import {Link} from "react-router-dom"
-export const NotFound=()=>{
-    return(
-        <div>
-            <h1> ...ez az oldal nem található...</h1>
-            <Link to="/">vissza ...</Link>
-        </div>
-    )
-}
