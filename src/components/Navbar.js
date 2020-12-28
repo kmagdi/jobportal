33d241ebba6dd1c097e9b1d1840a394b29f61dc7
kmagdi <katay.magdi@gmail.com>
2020-12-28 11:02:43 +0100

    responsive font size

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,15 @@ import { Link } from 'react-router-dom';
 import { debounce } from './debounce';  
 import "./Navbar.css"
 import logo from "../ikon.ico"
+import {
+  createMuiTheme,
+  responsiveFontSizes,
+  MuiThemeProvider,
+  Typography
+} from "@material-ui/core";
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
 
 
 
@@ -29,15 +38,21 @@ export const Navbar = ({showLink}) => {
     backgroundColor: '#333D79FF',
     color:'#FAEBEFFF',
     textAlign: 'center',
-    transition: 'top 0.6s'
-    
+    transition: 'top 0.6s',
+   
   }
 
   return (
     <div style={{ ...navbarStyles, top: visible ? '0' : '-100px' }}>  
       <div className="row">
           <div className="col-2 text-left pl-5 pt-2"><img src={logo} alt='logo' width={'50px'}/></div>
-          <div className="col-8 text-center pt-2"><h4>  Kecskeméti Szakképzési Centrum  </h4> </div>
+          <div className="col-8 text-center pt-2 myNav">
+          <MuiThemeProvider theme={theme}>
+            <Typography variant="subtitle" gutterBottom>
+              Kecskeméti Szakképzési Centrum  
+            </Typography>
+          </MuiThemeProvider>
+          </div>
            <div className="col-2">
               <Link to="/"  style={{ color: '#FAEBEFFF',textDecoration:"underline #FAEBEFFF" }}>     
                   <small className={showLink ? '': 'hidden'} >vissza a főoldalra...</small>
@@ -45,7 +60,15 @@ export const Navbar = ({showLink}) => {
             </div>
       </div>
       <div className="row">
-        <div className="col-12 text-center">Állásportál (gyakorlati projektfeladat)</div> 
+        <div className="col-12 text-center">
+          <MuiThemeProvider>
+            <Typography variant="subtitle" gutterBottom>
+              Állásportál (gyakorlati projektfeladat)
+            </Typography>
+          </MuiThemeProvider>
+          
+
+        </div> 
    </div>
        
     </div>
